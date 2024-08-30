f658b9cc70f240b72319f70f8514e0e85996aa22
Toni Deplano <montana.toni@gmail.com>
2024-08-30 23:43:45 +0200

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+  
+
 export default defineConfig({
+  base: '/commerce/',
   plugins: [react()],
   resolve: {
     alias: {
