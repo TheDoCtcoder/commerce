e7862bda957b1d8353e84eb5ac5a2940341935db
Toni Deplano <montana.toni@gmail.com>
2024-09-01 14:54:10 +0200

    fix redirect signout

--- src/main.jsx
+++ src/main.jsx
@@ -39,7 +39,7 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/commerce/">
     <RouterProvider router={router}/>
     </ClerkProvider>
     
