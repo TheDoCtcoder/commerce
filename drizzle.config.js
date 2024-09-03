fc72bc25af04450a2ce865b5b022babb83a86093
Toni Deplano <montana.toni@gmail.com>
2024-09-03 09:52:44 +0200

    config

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./configs/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://car-marketplace_owner:XjcL2T4QDhko@ep-long-shadow-a2tfg520.eu-central-1.aws.neon.tech/car-marketplace?sslmode=require',
-    }
-  };
