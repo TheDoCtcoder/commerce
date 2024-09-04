3556ca37703e7fc65406083e4330028dc2608a05
Toni Deplano <montana.toni@gmail.com>
2024-09-05 00:44:18 +0200

    logo change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,12 @@ function Header() {
   const { user, isSignedIn } = useUser();
   return (
     <div className='flex  justify-between  items-center  shadow-sm p-5'>
-      <img src="logo.svg" alt="logo" width={150} height={100} />
+      <div className='flex gap-5 items-center'>
+      {/* <img src="logo.svg" alt="logo" width={150} height={100} /> */}
+      <img src="peugeot5082.png" alt="logo" width={40} height={40} />
+      <h2 className=' font-medium'>Vente&Location</h2>
+      </div>
+      
 
       <ul className='  hidden md:flex gap-16'>
         <li className=' font-medium  hover:scale-105  transition-all cursor-pointer  hover:text-primary'><Link to={'/'} >Accueil</Link></li>
